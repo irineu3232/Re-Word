79b3c0567a19fe098da1aa49c773ae94d1f00fe7
Paulo <irineu32323@gmail.com>
2025-05-15 23:46:01 -0300

    Passo 6 completo, criando as controllers

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Re_World.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
