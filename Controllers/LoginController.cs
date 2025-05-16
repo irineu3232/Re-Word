9ccfc7bf4c39b81f4c9e9cea27f09b6a35f77d7f
Paulo <irineu32323@gmail.com>
2025-05-15 23:51:03 -0300

    Passo 7 completo, Adicionando as Views... sem o video

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Re_World.Repositorio;
+using Re_World.Models;
 
 namespace Re_World.Controllers
 {
-    public class LoginController : Controller
-    {
-        public IActionResult Index()
+   
+        public class LoginController : Controller
         {
-            return View();
+            private readonly LoginRepositorio _loginRepositorio;
+
+            public LoginController(LoginRepositorio loginRepositorio)
+            {
+                _loginRepositorio = loginRepositorio;
+            }
+
+
+            public IActionResult Login()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Login(string Email, string senha)
+            {
+                var usuario = _loginRepositorio.ObterUsuario(Email);
+                if (usuario != null && usuario.Senha == senha)
+                {
+                    return RedirectToAction("Index", "Produto");
+                }
+
+                ModelState.AddModelError("", "Erro, informações invalidas");
+                return View();
+            }
         }
-    }
 }
+
--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,12 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Re_World.Models;
+using Re_World.Repositorio;
 
 namespace Re_World.Controllers
 {
     public class ProdutoController : Controller
     {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
         public IActionResult Index()
         {
+            return View(_produtoRepositorio.TodosProdutos());
+        }
+
+
+
+        public IActionResult CadastrarProduto()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProduto(Produtos produto)
+        {
+            _produtoRepositorio.Cadastrar(produto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditarProduto(int id)
+        {
+            var produto = _produtoRepositorio.ObterProduto(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditarProduto(int id, [Bind("Id, Nome,Descricao,Preco,Qauntidade")] Produtos produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_produtoRepositorio.Atualizar(produto))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Ocorreu um erro ao editar.");
+                    return View(produto);
+                }
+            }
+            return View(produto);
+        }
+
+        public IActionResult ExcluirProduto(int id)
+        {
+            _produtoRepositorio.Excluir(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
