a6c72d385a4ee996e80f4633ee49785a2c7f726e
Paulo <irineu32323@gmail.com>
2025-05-15 23:41:00 -0300

    Passo 4 completo criando as models

---
+++ Models/Produtos.cs
@@ -0,0 +1,17 @@
+﻿namespace Re_World.Models
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public decimal? Preco { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public List<Produtos>? ListaProdutos { get; set; }
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿namespace Re_World.Models
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Senha { get; set; }
+    }
+}
